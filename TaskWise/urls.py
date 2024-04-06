f6c43defa7a8febd4507de7a463bb0ac64408193
amit-eiitech <amit.haldar@eiitechsolutions.com>
2024-04-06 13:09:07 +0900

    updated

--- TaskWise/urls.py
+++ TaskWise/urls.py
@@ -18,11 +18,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from . import views as mainViews
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html")),
-    # path("", mainViews.index),
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
